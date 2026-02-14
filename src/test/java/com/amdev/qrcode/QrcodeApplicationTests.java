762745b90b4e16a49d1a685ff38d5605b56c5021
alisson-moura <alisson.mo.moura@outlook.com.br>
2026-02-14 11:49:39 -0300

    chore: adiciona dependecias

---
+++ src/test/java/com/amdev/qrcode/QrcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amdev.qrcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
